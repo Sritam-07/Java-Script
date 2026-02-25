8dc2f75da60cb7698da363ec5d3ea632d606d78c
Sritam Das <dsritam04@gmail.com>
2026-02-25 16:45:32 +0000

    Variables and Constant

---
+++ Basics/prog2.js
@@ -0,0 +1,14 @@
+const accountId = 1234235
+let accEmail ="sritam008@gmail.com"
+var accountPassword = "12345"
+accountCity="kjr"
+
+
+console.log(accountId)//not allowed
+accountPassword="1234"
+// accountId = 12
+accEmail="pritam@0087"
+/*
+prefer not to use var use let 
+because of an issue due to scope of variable
+*/
